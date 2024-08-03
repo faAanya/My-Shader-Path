b7f4c5a71ac7dbffb25600f42bd75b1770cac50f
faAanya <108868725+faAanya@users.noreply.github.com>
2024-08-03 10:56:28 +0300

    wave shader

--- This is the work of art/Assets/Scenes/Building A Graf/GPUGraph.cs
+++ This is the work of art/Assets/Scenes/Building A Graf/GPUGraph.cs
@@ -1,10 +1,25 @@
+using System.Data;
 using UnityEngine;
 
 public class GPUGraph : MonoBehaviour
 {
 
+    [SerializeField]
+    ComputeShader computeShader;
 
-    [SerializeField, Range(10, 200)]
+    [SerializeField]
+    Material material;
+    [SerializeField]
+    Mesh mesh;
+
+    static readonly int
+    positionsId = Shader.PropertyToID("_Positions"),
+    resolutionId = Shader.PropertyToID("_Resolution"),
+    stepId = Shader.PropertyToID("_Step"),
+    timeId = Shader.PropertyToID("_Time");
+
+
+    [SerializeField, Range(0, 200)]
     int resolution = 10;
 
     [SerializeField]
@@ -13,7 +28,7 @@ public class GPUGraph : MonoBehaviour
     public enum TransitionMode { Cycle, Random }
 
     [SerializeField]
-    TransitionMode transitionMode;
+    TransitionMode transitionMode = TransitionMode.Cycle;
 
     [SerializeField, Min(0f)]
     float functionDuration = 1f, transitionDuration = 1f;
@@ -28,7 +43,39 @@ public class GPUGraph : MonoBehaviour
     ComputeBuffer positionBuffer;
 
 
+    void OnEnable()
+    {
+        positionBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+    }
 
+
+    void OnDisable()
+    {
+        positionBuffer.Release();
+        positionBuffer = null;
+    }
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+
+        computeShader.SetBuffer(0, positionsId, positionBuffer);
+
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+
+        material.SetBuffer(positionsId, positionBuffer);
+        material.SetFloat(stepId, step);
+
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+
+        Graphics.DrawMeshInstancedProcedural(
+            mesh, 0, material, bounds, positionBuffer.count
+        );
+    }
     void Update()
     {
         duration += Time.deltaTime;
@@ -48,6 +95,7 @@ public class GPUGraph : MonoBehaviour
             PickNextFunction();
         }
 
+        UpdateFunctionOnGPU();
 
     }
 
